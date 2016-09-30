a113be1bc4c9d9e009b3ad00d48a8a107501d8f7
ivanovvladimirv <development@ivanov.site>
2016-09-30 08:33:49 +0300

    fix

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
     <input type="text" name="name" maxlength="30">
     <input type="text" name="phone" required minlength="10" maxlength="20">
     <button type="submit">Заказать</button>
-    <a style="display:block" href="./second.html">Go to second page</a><br>
 </form>
 <!-- DrCash -->
 <script type="text/javascript">function orderSuccess(a){console.log(a['conversions'] ? a['conversions'] : '');window.location="./thanks.php"+enURL(a['conversions'][0] ? a['conversions'][0] : '');} function orderError(a){console.log(a)} window.onload = function() { order(); autotid('http://b.tracking.drcash.me/?offer_id=8&url_id=384&aff_id=1002'); metrica(location.search.replace("?","&") + '&host=' + location.host + location.pathname); } </script>
